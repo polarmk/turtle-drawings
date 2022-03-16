1ea8023efd506502dddf8430b1914ad69d4f6a7a
polarmk <101069711+polarmk@users.noreply.github.com>
2022-03-16 18:25:52 -0500

    Add files via upload

---
+++ turtle-b.py
@@ -0,0 +1,10 @@
+import turtle
+import time
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.circle(50, 180)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.circle(50, 180)
