fad55df150c32e98fc692da7b72f75d3868fc2d0
polarmk <101069711+polarmk@users.noreply.github.com>
2022-03-17 18:46:15 -0500

    LETTER: H

---
+++ turtle-h.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(180)
+turtle.forward(100)
